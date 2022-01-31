42d965108ea0b42bd0900e885c5545e41bd97f13
Tyler Scott <TylerS@epiloglaser.com>
2022-01-31 09:57:02 -0700

    Added tests for real.rs and fixed bug

--- src/real.rs
+++ src/real.rs
@@ -8,7 +8,6 @@ use crate::{Point};
 pub fn epsilon_for_value(num: f64) -> f64 {
     // These values have been obtained experimentally and can be changed if
     // necessary.
-    // (num.abs() * 0.0000000001).max(0.0000001)
     (num.abs() * 0.0000000001).max(f64::EPSILON)
 }
 
@@ -23,7 +22,13 @@ pub fn epsilon_for_point(pt: Point) -> f64 {
 /// Compare if two real values are approximately equal
 pub fn real_is_equal(num1: f64, num2: f64) -> bool
 {
-    (num1 - num2).abs() < epsilon_for_value(num1.max(num2))
+    if num1 == f64::INFINITY || num2 == f64::INFINITY
+    {
+        return num1 == num2; // Do exact comparison
+    }
+
+    // Do approximate comparison
+    (num1 - num2).abs() <= epsilon_for_value(num1.max(num2))
 }
 
 /// Compare if a  real value is approximately zero
@@ -60,4 +65,56 @@ pub fn real_gte(num1: f64, num2: f64) -> bool
 pub fn point_is_equal(pt1: Point, pt2: Point) -> bool
 {
     real_is_equal(pt1.x, pt2.x) && real_is_equal(pt1.y, pt2.y)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_real_comparisons() {
+        // Equals
+        assert!(real_is_equal(1., 1.));
+        assert!(real_is_equal(1000000., 1000000.));
+        assert!(real_is_equal(f64::INFINITY, f64::INFINITY));
+        assert!(!real_is_equal(f64::INFINITY, f64::NEG_INFINITY));
+        assert!(real_is_equal(f64::EPSILON, f64::EPSILON));
+        assert!(!real_is_equal(f64::EPSILON, -f64::EPSILON));
+        assert!(real_is_equal(f64::EPSILON, 0.));
+        assert!(real_is_equal(0., f64::EPSILON));
+
+        // Less than
+        assert!(real_lte(1., 1.));
+        assert!(real_lte(1000000., 1000000.));
+        assert!(real_lte(f64::INFINITY, f64::INFINITY));
+        assert!(!real_lt(1., 1.));
+        assert!(!real_lt(1000000., 1000000.));
+        assert!(!real_lt(f64::INFINITY, f64::INFINITY));
+        assert!(real_lt(0., f64::INFINITY));
+        assert!(real_lt(f64::NEG_INFINITY, 0.));
+        assert!(real_lt(-1., 1.));
+        assert!(real_lt(f64::NEG_INFINITY, f64::INFINITY));
+
+        // Greater than
+        assert!(real_gte(1., 1.));
+        assert!(real_gte(1000000., 1000000.));
+        assert!(real_gte(f64::INFINITY, f64::INFINITY));
+        assert!(!real_gt(1., 1.));
+        assert!(!real_gt(1000000., 1000000.));
+        assert!(!real_gt(f64::INFINITY, f64::INFINITY));
+        assert!(real_gt(f64::INFINITY, 0.));
+        assert!(real_gt(0., f64::NEG_INFINITY));
+        assert!(real_gt(1., -1.));
+        assert!(real_gt(f64::INFINITY, f64::NEG_INFINITY));
+    }
+
+    #[test]
+    fn test_point_comparisons() {
+        assert!(point_is_equal(Point::new(0., 0.), Point::new(0., 0.)));
+        assert!(point_is_equal(Point::new(1000000., 1000000.), Point::new(1000000., 1000000.)));
+        assert!(point_is_equal(Point::new(f64::INFINITY, f64::INFINITY), Point::new(f64::INFINITY, f64::INFINITY)));
+        assert!(!point_is_equal(Point::new(0., 0.), Point::new(1., 1.)));
+        assert!(!point_is_equal(Point::new(f64::INFINITY, f64::INFINITY), Point::new(f64::NEG_INFINITY, f64::NEG_INFINITY)));
+        assert!(point_is_equal(Point::new(0., 0.), Point::new(f64::EPSILON, f64::EPSILON)));
+    }
 }
